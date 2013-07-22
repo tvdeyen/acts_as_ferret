2bfaafc1bdaebba2216a45138f878bd9bee139b8
Thomas von Deyen <tvdeyen@gmail.com>
2013-07-22 21:14:41 +0200

    Put log_file module in acts_as_ferret module

--- lib/acts_as_ferret/log_file.rb
+++ lib/acts_as_ferret/log_file.rb
@@ -1,13 +1,15 @@
-module ActsAsFerret::LogFile
-  LOG_FILE_NAME = 'acts_as_ferret.log'
+module ActsAsFerret
+  module LogFile
+    LOG_FILE_NAME = 'acts_as_ferret.log'
 
-  def self.get_or_create_log_file
-    logfile = "#{Rails.root || '.'}/log/#{LOG_FILE_NAME}"
-    unless File.exists?(logfile)
-      FileUtils.mkdir_p('log')
-      FileUtils.touch(logfile)
+    def self.get_or_create_log_file
+      logfile = "#{Rails.root || '.'}/log/#{LOG_FILE_NAME}"
+      unless File.exists?(logfile)
+        FileUtils.mkdir_p('log')
+        FileUtils.touch(logfile)
+      end
+      logfile
     end
-    logfile
-  end
 
+  end
 end
